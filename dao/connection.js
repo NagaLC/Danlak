cfc6b9e6b5d61a0d70b82388dc4cd5ceb5de9e38
NagaLC <kriss.taler@gmail.com>
2019-04-20 15:07:10 +0200

    remove config.json from connection.js

--- dao/connection.js
+++ dao/connection.js
@@ -1,5 +1,4 @@
 const db_mysql = require('mysql');
-const { db_host, db_user, db_password, db_database } = require('./../config.json');
 
 const connection = db_mysql.createConnection({
 	host: process.env.HOST,
